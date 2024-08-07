2496f39cf63d0ff68fb55fb8d80c93830bb2a782
Josh Burt <joshburt@Joshs-MacBook-Pro.local>
2024-08-06 18:31:56 -0700

    baseline

--- src/shapeandshare/light/const.py
+++ src/shapeandshare/light/const.py
@@ -8,7 +8,8 @@ BLACK: tuple[int, int, int] = (0, 0, 0)
 WHITE: tuple[int, int, int] = (255, 255, 255)
 
 # dimensions=(20, 20) 20x15=300, 20x15=300
-WINDOW_WIDTH: int = 600
+
+WINDOW_WIDTH: int = 1200
 WINDOW_HEIGHT: int = 600
 
 FPS: int = 60
--- src/shapeandshare/light/display.py
+++ src/shapeandshare/light/display.py
@@ -1,12 +1,14 @@
 import pygame
 from pygame import Surface, SurfaceType
 
-from .const import PRODUCT_NAME, WINDOW_HEIGHT, WINDOW_WIDTH
+from .const import PRODUCT_NAME
 
 
 def build_display(width: int, height: int) -> Surface | SurfaceType:
     display_surface: Surface | SurfaceType = pygame.display.set_mode(
-        (width, height), flags=pygame.RESIZABLE | pygame.SCALED
+        (width, height),
+        flags=pygame.RESIZABLE | pygame.SCALED,
+        # (width, height), flags=pygame.RESIZABLE
     )
     pygame.display.set_caption(PRODUCT_NAME)
     return display_surface
